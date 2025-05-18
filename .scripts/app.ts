3c3c57efcd6fd26dd3ab36c5278b18ad82ef26d4
Asaki Yuki <vantrong2007vn@gmail.com>
2025-05-18 23:42:42 +0700

    auto remove

--- .scripts/app.ts
+++ .scripts/app.ts
@@ -10,20 +10,21 @@ const workFolder = process.cwd();
 const folders = fs.readdirSync(workFolder).filter((file) => {
     const filePath = `${workFolder}/${file}`;
     const stat = fs.statSync(filePath);
-    if (stat.isDirectory() && !existVideos.includes(file)) {
+    if (stat.isDirectory()) {
         if (!(file === "node_modules" || file.startsWith("."))) return true;
     } return false;
 })
 
 const videos: string[] = []
 
-for (const video of folders) {
+for (const video of folders.filter((file) => !existVideos.includes(file))) {
     const videoPath = `${workFolder}/${video}`;
     if (fs.existsSync(`${videoPath}/metadata.json`) && fs.existsSync(`${videoPath}/video.mp4`)) {
         videos.push(video)
-        execSync(`ffmpeg -i "${videoPath}/video.mp4" -frames 1 -q:v 2 "${videoPath}/thumbnail.jpg"`, { stdio: "inherit" });
+        if (!fs.existsSync(`${videoPath}/thumbnail.jpg`))
+            execSync(`ffmpeg -i "${videoPath}/video.mp4" -frames 1 -q:v 2 "${videoPath}/thumbnail.jpg"`, { stdio: "inherit" });
     };
 }
 
 fs.writeFileSync("commit", videos.length ? `New videos added: ${videos.length}` : "");
-fs.writeFileSync("exist.json", JSON.stringify({ videos: [...existVideos, ...videos] }, null, 2));
+fs.writeFileSync("exist.json", JSON.stringify({ videos: [...existVideos, ...videos].filter(file => folders.includes(file)) }, null, 2));
